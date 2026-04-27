b5a9516e74c9f0fe514cd61366fd38fdc4e1c14a
Nderitu-Dennis <nderitudenis87@gmail.com>
2026-04-27 12:30:46 +0300

    coded to an interface, Map<k,v> instead of HashMap<k,v> read comment notes above to understand more

--- src/collections/hashmaps/Student.java
+++ src/collections/hashmaps/Student.java
@@ -1,19 +1,43 @@
 package collections.hashmaps;
 /* HashMaps- same as dictionary in python. Stores data in key-value pairs
  and keys must be unique, no duplicate keys. used whenever fast lookup is needed
-   cz u just search using the key. */
+   cz u just search using the key.
+
+   2. Map vs. HashMap (The Variable Declaration)
+You asked about the difference between these two ways of writing code:
+
+Option A: Map<K, V> map = new HashMap<>(); (The Professional Way)
+This is called coding to an interface. It makes your code flexible.
+Benefit: If you realize later that you need the map to stay sorted, you only have to change one word:
+ Map<K, V> map = new TreeMap<>();.
+- The rest of your code won't break because it only cares that the object behaves like a Map.
+
+Option B: HashMap<K, V> map = new HashMap<>();
+This ties your variable specifically to a HashMap.
+- Downside: You can only ever use HashMap methods. If you try to swap it for a TreeMap later,
+the compiler might complain if you used any HashMap-specific shortcuts elsewhere in your code.
+
+- you must choose a concrete class to do the actual work:
+1. HashMap: Best for general use (fastest).
+2. LinkedHashMap: Use this if you need to remember the order in which you added items.
+3. TreeMap: Use this if you want the keys to be alphabetical or numerical.
+4. WeakHashMap: A specialized map where entries can be removed by the "Garbage Collector"
+ automatically (used for caching).*/
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Student {
     public static void main(String[] args) {
-        HashMap<String, Integer> studentMarks = new HashMap<>();
+        Map<String, Integer> studentMarks = new HashMap<>();
 
-        studentMarks.put("Dennis the legend", 40);
+        studentMarks.put("Dennis the legend", 92);
         studentMarks.put("Allan Njogothe", 92);
         studentMarks.put("Kelvin the lover boy", 76);
         studentMarks.put("Sunil mohammed", 48);
         studentMarks.put("Greyson marcus", 10);
+
+
 
 //        printing the marks
         System.out.println("\n" + studentMarks);
